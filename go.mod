83ad24114167e57c51a1715183f7ab687a874713
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2024-10-31 10:06:43 -0600

    Implement Connect() and Disconnect() for VMwareSource
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FuturFusion/migration-manager
 
 go 1.23.2
+
+require github.com/vmware/govmomi v0.45.1
--- internal/source/vmware.go
+++ internal/source/vmware.go
@@ -2,6 +2,12 @@ package source
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/vmware/govmomi/session/cache"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
 )
 
 // VMwareSource defines a VMware endpoint that the migration manager can connect to.
@@ -25,6 +31,9 @@ type VMwareSource struct {
 	// If true, disable TLS certificate validation
 	// Example: false
 	Insecure bool `json:"insecure" yaml:"insecure"`
+
+	vimClient  *vim25.Client
+	vimSession *cache.Session
 }
 
 // Returns a new VMwareSource ready for use.
@@ -42,11 +51,44 @@ func NewVMwareSource(name string, endpoint string, username string, password str
 }
 
 func (s *VMwareSource) Connect(ctx context.Context) error {
+	if s.isConnected {
+		return fmt.Errorf("Already connected to endpoint '%s'", s.Endpoint)
+	}
+
+	endpointURL, err := soap.ParseURL(s.Endpoint)
+	if err != nil {
+		return err
+	}
+
+	endpointURL.User = url.UserPassword(s.Username, s.Password)
+
+	s.vimSession = &cache.Session{
+		URL:      endpointURL,
+		Insecure: s.Insecure,
+	}
+
+	s.vimClient = new(vim25.Client)
+	err = s.vimSession.Login(ctx, s.vimClient, nil)
+	if err != nil {
+		return err
+	}
+
 	s.isConnected = true
 	return nil
 }
 
 func (s *VMwareSource) Disconnect(ctx context.Context) error {
+	if !s.isConnected {
+		return fmt.Errorf("Not connected to endpoint '%s'", s.Endpoint)
+	}
+
+	err := s.vimSession.Logout(ctx, s.vimClient)
+	if err != nil {
+		return err
+	}
+
+	s.vimClient = nil
+	s.vimSession = nil
 	s.isConnected = false
 	return nil
 }
