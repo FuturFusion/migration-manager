6d2d5763be3d8bfe1dac426823675f483cc6527a
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2024-10-31 16:05:28 -0600

    Load agent config from file
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- cmd/migration-manager-agent/main.go
+++ cmd/migration-manager-agent/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/FuturFusion/migration-manager/internal"
+	"github.com/FuturFusion/migration-manager/internal/agent"
 )
 
 func main() {
-	fmt.Printf("Hello, world\n")
+	fmt.Printf("This is migration-manager-agent v%s\n", internal.Version)
+
+	// TODO -- Fetch this file from the config drive that's mounted into the VM
+	agentConfig, err := agent.AgentConfigFromYamlFile("./agent.yaml")
+	if err != nil {
+		fmt.Printf("Failed to open config file: %s\n", err)
+		os.Exit(1)
+	}
 }
