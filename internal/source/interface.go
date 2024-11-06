89f3af8ccb7cd0993a8ccee1c40be40741db91db
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2024-11-06 10:43:20 -0700

    Document functions in interfaces
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- internal/source/interface.go
+++ internal/source/interface.go
@@ -13,13 +13,42 @@ const (
 
 // Interface definition for all migration manager sources.
 type Source interface {
+	// Connects to the source, using any source-specific details when the object was created.
+	//
+	// Returns an error if unable to connect (unable to reach remote host, bad credentials, etc).
 	Connect(ctx context.Context) error
+
+	// Disconnects from the source.
+	//
+	// Returns an error if there was a problem disconnecting from the source.
 	Disconnect(ctx context.Context) error
+
+	// Returns whether currently connected to the source or not.
 	IsConnected() bool
 
-	DeleteVMSnapshot(ctx context.Context, vmName string, snapshotName string) error
-	ImportDisks(ctx context.Context, vmName string) error
+//////////////////////////////////////////////////
 
+	// Returns the human-readable name for this source.
 	GetName() string
+
+	// Returns a unique ID for this source that can be used when interacting with the database.
+	//
+	// A freshly-created source that hasn't yet been added to the database via AddSource() or
+	// retrieved via GetSource()/GetAllSources() will return an invalid ID.
 	GetDatabaseID() int
+
+//////////////////////////////////////////////////
+
+	// Deletes a given snapshot, if it exists, from the specified VM.
+	//
+	// Returns an error if there is a problem deleting the snapshot.
+	DeleteVMSnapshot(ctx context.Context, vmName string, snapshotName string) error
+
+	// Initiates a disk import cycle from the source to the locally running VM.
+	//
+	// Important: This should only be called from the migration manager worker, as it will attempt to
+	// directly write to raw disk devices, overwriting any data that might already be present.
+	//
+	// Returns an error if there is a problem importing the disk(s).
+	ImportDisks(ctx context.Context, vmName string) error
 }
--- internal/target/interface.go
+++ internal/target/interface.go
@@ -6,15 +6,60 @@ import (
 
 // Interface definition for all migration manager targets.
 type Target interface {
+	// Connects to the target.
+	//
+	// Prior to calling Connect(), set any other configuration required, such as the client's TLS
+	// credentials if not using OIDC authentication.
+	//
+	// Returns an error if unable to connect (unable to reach remote host, bad credentials, etc).
 	Connect(ctx context.Context) error
+
+	// Disconnects from the target.
+	//
+	// Returns an error if there was a problem disconnecting from the target.
 	Disconnect(ctx context.Context) error
+
+	// Toggles whether TLS verification should be skipped or not.
+	//
+	// As this can enable MITM-style attacks, in general this SHOULD NOT be used.
+	//
+	// Returns an error if called while connected to a target.
 	SetInsecureTLS(insecure bool) error
+
+	// Sets the client TLS key and certificate to be used to authenticate with the target. Leave unset to
+	// default to OIDC authentication.
+	//
+	// The key/cert pair can be generated with a command like
+	// `openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -sha384 -keyout "client.key" -nodes -out "client.crt" -days 365 -subj "/"`.
+	// The resulting certificate can then be imported on the target instance with
+	// `incus config trust add-certificate <client.crt>`.
+	//
+	// Returns an error if called while connected to a target.
 	SetClientTLSCredentials(key string, cert string) error
+
+	// Returns whether currently connected to the target or not.
 	IsConnected() bool
 
+	//////////////////////////////////////////////////
+
+	// Returns the human-readable name for this target.
 	GetName() string
+
+	// Returns a unique ID for this target that can be used when interacting with the database.
+	//
+	// A freshly-created target that hasn't yet been added to the database via AddTarget() or
+	// retrieved via GetTarget()/GetAllTargets() will return an invalid ID.
 	GetDatabaseID() int
 
+	//////////////////////////////////////////////////
+
+	// Selects the Incus profile to use when performing actions on the target.
+	//
+	// Returns an error if called while disconnected from a target.
 	SetProfile(profile string) error
+
+	// Selects the Incus project to use when performing actions on the target.
+	//
+	// Returns an error if called while disconnected from a target.
 	SetProject(project string) error
 }
