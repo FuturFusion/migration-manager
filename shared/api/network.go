71d1f9362522a3d3ba913595885b40b960e33176
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2024-11-22 09:13:30 -0700

    Add Network struct
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

---
+++ shared/api/network.go
@@ -0,0 +1,18 @@
+package api
+
+// Network defines the network config for use by the migration manager.
+//
+// swagger:model
+type Network struct {
+	// The name of the network
+	// Example: network-23
+	Name string `json:"name" yaml:"name"`
+
+	// An opaque integer identifier for the network
+	// Example: 123
+	DatabaseID int `json:"databaseID" yaml:"databaseID"`
+
+	// Any network-specific config options
+	// Example: {"network": "vmware", "ipv6.address": "none"}
+	Config map[string]string `json:"config" yaml:"config"`
+}
