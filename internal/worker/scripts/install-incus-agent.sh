bd9a9b50a336b9d8461280512572a0b37623f9a0
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2024-12-04 09:29:31 +0100

    Fix shellcheck findings
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/worker/scripts/install-incus-agent.sh
+++ internal/worker/scripts/install-incus-agent.sh
@@ -3,7 +3,7 @@
 # Install incus-agent into the target system.
 mkdir -p /mnt/config/
 mount -t 9p config /mnt/config/
-cd /mnt/config/
+cd /mnt/config/ || exit 1
 ./install.sh
-cd
+cd || exit 1
 umount /mnt/config/
