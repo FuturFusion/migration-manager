4d508f8f776cb6273c0ce87860306b300ab9e342
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2024-12-05 11:43:40 +0100

    Refactor tests
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- cmd/migration-managerd/api_target_test.go
+++ cmd/migration-managerd/api_target_test.go
@@ -36,43 +36,15 @@ func TestTargetsGet(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			// Setup
-			var err error
-
-			tmpDir := t.TempDir()
-
-			daemon := newDaemon(&DaemonConfig{})
-			daemon.db, err = db.OpenDatabase(tmpDir)
-			require.NoError(t, err)
-
-			router := mux.NewRouter()
-			daemon.createCmd(router, "1.0", targetsCmd)
-			srv := httptest.NewServer(router)
-			defer srv.Close()
-
-			daemon.db.Transaction(context.Background(), func(ctx context.Context, tx *sql.Tx) error {
-				return daemon.db.AddTarget(tx, &target.InternalIncusTarget{
-					IncusTarget: api.IncusTarget{
-						Name:     "foo",
-						Endpoint: "bar",
-						Insecure: false,
-					},
-				})
-			})
-			require.NoError(t, err)
+			daemon, srvURL := daemonSetup(t, []APIEndpoint{targetsCmd})
+			seedDBWithSingleTarget(t, daemon)
 
 			// Execute test
-			resp, err := http.Get(srv.URL + "/1.0/targets")
-			require.NoError(t, err)
+			statusCode, body := probeAPI(t, http.MethodGet, srvURL+"/1.0/targets", http.NoBody)
 
 			// Assert results
-			require.Equal(t, tc.wantHTTPStatus, resp.StatusCode)
-
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			require.NoError(t, err)
-
-			require.Equal(t, tc.wantTargetCount, gjson.GetBytes(body, "metadata.#").Int())
+			require.Equal(t, tc.wantHTTPStatus, statusCode)
+			require.Equal(t, tc.wantTargetCount, gjson.Get(body, "metadata.#").Int())
 		})
 	}
 }
@@ -124,39 +96,130 @@ func TestTargetDelete(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			// Setup
-			var err error
-
-			tmpDir := t.TempDir()
-
-			daemon := newDaemon(&DaemonConfig{})
-			daemon.db, err = db.OpenDatabase(tmpDir)
-			require.NoError(t, err)
-
-			router := mux.NewRouter()
-			daemon.createCmd(router, "1.0", targetCmd)
-			srv := httptest.NewServer(router)
-			defer srv.Close()
-
-			daemon.db.Transaction(context.Background(), func(ctx context.Context, tx *sql.Tx) error {
-				return daemon.db.AddTarget(tx, &target.InternalIncusTarget{
-					IncusTarget: api.IncusTarget{
-						Name:     "foo",
-						Endpoint: "bar",
-						Insecure: false,
-					},
-				})
-			})
-			require.NoError(t, err)
+			daemon, srvURL := daemonSetup(t, []APIEndpoint{targetCmd})
+			seedDBWithSingleTarget(t, daemon)
 
 			// Execute test
-			req, err := http.NewRequest(http.MethodDelete, srv.URL+fmt.Sprintf("/1.0/targets/%s", tc.targetName), http.NoBody)
-			require.NoError(t, err)
-
-			resp, err := http.DefaultClient.Do(req)
-			require.NoError(t, err)
+			statusCode, _ := probeAPI(t, http.MethodDelete, srvURL+fmt.Sprintf("/1.0/targets/%s", tc.targetName), http.NoBody)
 
 			// Assert results
-			require.Equal(t, tc.wantHTTPStatus, resp.StatusCode)
+			require.Equal(t, tc.wantHTTPStatus, statusCode)
 		})
 	}
+}
+
+func TestTargetGet(t *testing.T) {
+	tests := []struct {
+		name string
+
+		targetName string
+
+		wantHTTPStatus int
+		wantTargetName string
+	}{
+		{
+			name: "success",
+
+			targetName: "foo",
+
+			wantHTTPStatus: http.StatusOK,
+			wantTargetName: "foo",
+		},
+		{
+			name: "error - empty name",
+
+			targetName: "",
+
+			// TODO: the business logic would like to return http.BadRequest for this
+			// but this gets never hit, because the router is already handling this
+			// request with http.StatusNotFound.
+			wantHTTPStatus: http.StatusNotFound,
+		},
+		{
+			name: "error - empty name (with final slash)",
+
+			targetName: "/",
+
+			// TODO: the business logic would like to return http.BadRequest for this
+			// but this gets never hit, because the router is already handling this
+			// request with http.StatusNotFound.
+			wantHTTPStatus: http.StatusNotFound,
+		},
+		{
+			name: "error - not found",
+
+			targetName: "invalid_name",
+
+			wantHTTPStatus: http.StatusBadRequest,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup
+			daemon, srvURL := daemonSetup(t, []APIEndpoint{targetCmd})
+			seedDBWithSingleTarget(t, daemon)
+
+			// Execute test
+			statusCode, body := probeAPI(t, http.MethodGet, srvURL+fmt.Sprintf("/1.0/targets/%s", tc.targetName), http.NoBody)
+
+			// Assert results
+			require.Equal(t, tc.wantHTTPStatus, statusCode)
+			require.Equal(t, tc.wantTargetName, gjson.Get(body, "metadata.name").String())
+		})
+	}
+}
+
+func probeAPI(t *testing.T, method string, url string, requestBody io.Reader) (statusCode int, responseBody string) {
+	t.Helper()
+
+	req, err := http.NewRequest(method, url, requestBody)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp.StatusCode, string(body)
+}
+
+func daemonSetup(t *testing.T, endpoints []APIEndpoint) (*Daemon, string) {
+	t.Helper()
+
+	var err error
+
+	tmpDir := t.TempDir()
+
+	daemon := newDaemon(&DaemonConfig{})
+	daemon.db, err = db.OpenDatabase(tmpDir)
+	require.NoError(t, err)
+
+	router := mux.NewRouter()
+	for _, cmd := range endpoints {
+		daemon.createCmd(router, "1.0", cmd)
+	}
+
+	srv := httptest.NewServer(router)
+	t.Cleanup(srv.Close)
+
+	return daemon, srv.URL
+}
+
+func seedDBWithSingleTarget(t *testing.T, daemon *Daemon) {
+	t.Helper()
+
+	err := daemon.db.Transaction(context.Background(), func(ctx context.Context, tx *sql.Tx) error {
+		return daemon.db.AddTarget(tx, &target.InternalIncusTarget{
+			IncusTarget: api.IncusTarget{
+				Name:     "foo",
+				Endpoint: "bar",
+				Insecure: false,
+			},
+		})
+	})
+	require.NoError(t, err)
 }
