23b9eea7036e3278556a458bee3a66f4784b8563
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2024-12-11 10:25:10 +0100

    Add lint check to ensure go generate
    Ensure generated code is up to date
    
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- scripts/lint/go-generate-up-to-date.sh
+++ scripts/lint/go-generate-up-to-date.sh
@@ -2,10 +2,9 @@
 
 go generate ./...
 
-if [ -n "$(git status --porcelain -- '**/*_gen.go')" ]; then
+if [ -n "$(git status --porcelain -- '**/*_gen.go' '**/*_gen_test.go')" ]; then
   git status -- '**/*_gen.go'
   exit 1
 fi
 
 exit 0
-
