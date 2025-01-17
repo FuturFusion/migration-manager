6ef14303dd72cf6844e4d1806a556b284bc40c23
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-01-17 10:24:23 +0100

    ui: Support for OIDC authentication
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,20 +1,35 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from 'react-router';
 import { Container } from 'react-bootstrap';
-import Sidebar from "./components/Sidebar";
-import Batch from "./pages/Batch.tsx"
-import Instance from "./pages/Instance.tsx"
-import Source from "./pages/Source.tsx"
-import Target from "./pages/Target.tsx"
-import Queue from "./pages/Queue.tsx"
+import Sidebar from 'components/Sidebar';
+import Batch from 'pages/Batch';
+import Home from 'pages/Home';
+import Instance from 'pages/Instance';
+import Source from 'pages/Source';
+import Target from 'pages/Target';
+import Queue from 'pages/Queue';
+import { useAuth } from 'context/auth';
 
 function App() {
+  const { isAuthenticated, isAuthLoading } = useAuth();
+
+  if (isAuthLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    if (window.location.pathname !== "/ui/") {
+      window.location.href = "/ui/";
+    }
+  }
+
   return (
     <>
     <div style={{ display: 'flex' }}>
       <Sidebar />
       <Container fluid style={{ paddingLeft: '30px', paddingTop: '30px', transition: 'padding-left 0.3s' }}>
         <Routes>
-          <Route index path="/ui/sources" element={<Source />} />
+          <Route path="/ui" element={<Home />} />
+          <Route path="/ui/sources" element={<Source />} />
           <Route path="/ui/targets" element={<Target />} />
           <Route path="/ui/instances" element={<Instance />} />
           <Route path="/ui/batches" element={<Batch />} />
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,22 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import {
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-import './css/index.css'
+} from '@tanstack/react-query';
+import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router";
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router';
+import App from './App.tsx';
+import { AuthProvider } from 'context/auth';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </AuthProvider>
   </QueryClientProvider>
 )
