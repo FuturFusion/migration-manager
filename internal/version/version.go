1a30c47e49f64f28b4d6c447a8ffd5ae9b26efb3
Max Asnaashari <max.asna@futurfusion.io>
2025-02-25 13:54:15 -0800

    internal/version: Add Go semantic version helper
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,8 @@
 package version
 
-const Version string = "0.0.1"
+const Version string = "0.0.0-pre.2"
+
+// GoVersion returns the project version formatted as a Go semantic version string.
+func GoVersion() string {
+	return "v" + Version
+}
