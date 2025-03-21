b75d45474c9b9dc5e74236ea73da0c127fd2e620
Max Asnaashari <max.asna@futurfusion.io>
2025-03-21 15:58:33 -0700

    internal/migration/repo/sqlite: Add repo wrappers
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/migration/repo/sqlite/network.go
+++ internal/migration/repo/sqlite/network.go
@@ -2,12 +2,11 @@ package sqlite
 
 import (
 	"context"
-	"database/sql"
-	"encoding/json"
-	"fmt"
 
 	"github.com/FuturFusion/migration-manager/internal/migration"
 	"github.com/FuturFusion/migration-manager/internal/migration/repo"
+	"github.com/FuturFusion/migration-manager/internal/migration/repo/sqlite/entities"
+	"github.com/FuturFusion/migration-manager/internal/transaction"
 )
 
 type network struct {
@@ -22,166 +21,32 @@ func NewNetwork(db repo.DBTX) *network {
 	}
 }
 
-func (n network) Create(ctx context.Context, in migration.Network) (migration.Network, error) {
-	const sqlInsert = `
-INSERT INTO networks (name, config)
-VALUES(:name, :config)
-RETURNING id, name, config;
-`
-
-	config, err := json.Marshal(in.Config)
-	if err != nil {
-		return migration.Network{}, fmt.Errorf("Failed to marshal network config: %w", err)
-	}
-
-	row := n.db.QueryRowContext(ctx, sqlInsert,
-		sql.Named("name", in.Name),
-		sql.Named("config", config),
-	)
-	if row.Err() != nil {
-		return migration.Network{}, mapErr(row.Err())
-	}
-
-	return scanNetwork(row)
+func (n network) Create(ctx context.Context, in migration.Network) (int64, error) {
+	return entities.CreateNetwork(ctx, n.db, in)
 }
 
 func (n network) GetAll(ctx context.Context) (migration.Networks, error) {
-	const sqlGetAll = `SELECT id, name, config FROM networks ORDER BY name;`
-
-	rows, err := n.db.QueryContext(ctx, sqlGetAll)
-	if err != nil {
-		return nil, mapErr(err)
-	}
-
-	defer func() { _ = rows.Close() }()
-
-	var networks migration.Networks
-	for rows.Next() {
-		network, err := scanNetwork(rows)
-		if err != nil {
-			return nil, err
-		}
-
-		networks = append(networks, network)
-	}
-
-	if rows.Err() != nil {
-		return nil, mapErr(rows.Err())
-	}
-
-	return networks, nil
+	return entities.GetNetworks(ctx, n.db)
 }
 
 func (n network) GetAllNames(ctx context.Context) ([]string, error) {
-	const sqlGetAllNames = `SELECT name FROM networks ORDER BY name;`
-
-	rows, err := n.db.QueryContext(ctx, sqlGetAllNames)
-	if err != nil {
-		return nil, mapErr(err)
-	}
-
-	defer func() { _ = rows.Close() }()
-
-	var networkNames []string
-	for rows.Next() {
-		var networkName string
-		err := rows.Scan(&networkName)
-		if err != nil {
-			return nil, mapErr(err)
-		}
-
-		networkNames = append(networkNames, networkName)
-	}
-
-	if rows.Err() != nil {
-		return nil, mapErr(rows.Err())
-	}
-
-	return networkNames, nil
+	return entities.GetNetworkNames(ctx, n.db)
 }
 
-func (n network) GetByID(ctx context.Context, id int) (migration.Network, error) {
-	const sqlGetByID = `SELECT id, name, config FROM networks WHERE id=:id;`
-
-	row := n.db.QueryRowContext(ctx, sqlGetByID, sql.Named("id", id))
-	if row.Err() != nil {
-		return migration.Network{}, mapErr(row.Err())
-	}
-
-	return scanNetwork(row)
+func (n network) GetByName(ctx context.Context, name string) (*migration.Network, error) {
+	return entities.GetNetwork(ctx, n.db, name)
 }
 
-func (n network) GetByName(ctx context.Context, name string) (migration.Network, error) {
-	const sqlGetByName = `SELECT id, name, config FROM networks WHERE name=:name;`
-
-	row := n.db.QueryRowContext(ctx, sqlGetByName, sql.Named("name", name))
-	if row.Err() != nil {
-		return migration.Network{}, mapErr(row.Err())
-	}
-
-	return scanNetwork(row)
+func (n network) Update(ctx context.Context, in migration.Network) error {
+	return transaction.ForceTx(ctx, n.db, func(ctx context.Context, tx transaction.TX) error {
+		return entities.UpdateNetwork(ctx, tx, in.Name, in)
+	})
 }
 
-func (n network) UpdateByID(ctx context.Context, in migration.Network) (migration.Network, error) {
-	const sqlUpsert = `
-UPDATE networks SET name=:name, config=:config
-WHERE id=:id
-RETURNING id, name, config;
-`
-
-	config, err := json.Marshal(in.Config)
-	if err != nil {
-		return migration.Network{}, fmt.Errorf("Failed to marshal network config: %w", err)
-	}
-
-	row := n.db.QueryRowContext(ctx, sqlUpsert,
-		sql.Named("name", in.Name),
-		sql.Named("config", config),
-		sql.Named("id", in.ID),
-	)
-	if row.Err() != nil {
-		return migration.Network{}, mapErr(row.Err())
-	}
-
-	return scanNetwork(row)
-}
-
-func scanNetwork(row interface{ Scan(dest ...any) error }) (migration.Network, error) {
-	var network migration.Network
-	var configJSON []byte
-	err := row.Scan(
-		&network.ID,
-		&network.Name,
-		&configJSON,
-	)
-	if err != nil {
-		return migration.Network{}, mapErr(err)
-	}
-
-	err = json.Unmarshal(configJSON, &network.Config)
-	if err != nil {
-		return migration.Network{}, fmt.Errorf("Failed to unmarshal network config: %w", err)
-	}
-
-	return network, nil
+func (n network) Rename(ctx context.Context, oldName string, newName string) error {
+	return entities.RenameNetwork(ctx, n.db, oldName, newName)
 }
 
 func (n network) DeleteByName(ctx context.Context, name string) error {
-	const sqlDelete = `DELETE FROM networks WHERE name=:name;`
-
-	result, err := n.db.ExecContext(ctx, sqlDelete, sql.Named("name", name))
-	if err != nil {
-		return mapErr(err)
-	}
-
-	affectedRows, err := result.RowsAffected()
-	if err != nil {
-		return mapErr(err)
-	}
-
-	if affectedRows == 0 {
-		return migration.ErrNotFound
-	}
-
-	return nil
+	return entities.DeleteNetwork(ctx, n.db, name)
 }
--- internal/migration/repo/sqlite/source.go
+++ internal/migration/repo/sqlite/source.go
@@ -2,10 +2,11 @@ package sqlite
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/FuturFusion/migration-manager/internal/migration"
 	"github.com/FuturFusion/migration-manager/internal/migration/repo"
+	"github.com/FuturFusion/migration-manager/internal/migration/repo/sqlite/entities"
+	"github.com/FuturFusion/migration-manager/internal/transaction"
 )
 
 type source struct {
@@ -20,153 +21,32 @@ func NewSource(db repo.DBTX) *source {
 	}
 }
 
-func (s source) Create(ctx context.Context, in migration.Source) (migration.Source, error) {
-	const sqlInsert = `
-INSERT INTO sources (name, source_type, properties)
-VALUES(:name, :source_type, :properties)
-RETURNING id, name, source_type, properties;
-`
-
-	row := s.db.QueryRowContext(ctx, sqlInsert,
-		sql.Named("name", in.Name),
-		sql.Named("source_type", in.SourceType),
-		sql.Named("properties", in.Properties),
-	)
-	if row.Err() != nil {
-		return migration.Source{}, mapErr(row.Err())
-	}
-
-	return scanSource(row)
+func (s source) Create(ctx context.Context, in migration.Source) (int64, error) {
+	return entities.CreateSource(ctx, s.db, in)
 }
 
 func (s source) GetAll(ctx context.Context) (migration.Sources, error) {
-	const sqlGetAll = `SELECT id, name, source_type, properties FROM sources ORDER BY name;`
-
-	rows, err := s.db.QueryContext(ctx, sqlGetAll)
-	if err != nil {
-		return nil, mapErr(err)
-	}
-
-	defer func() { _ = rows.Close() }()
-
-	var sources migration.Sources
-	for rows.Next() {
-		source, err := scanSource(rows)
-		if err != nil {
-			return nil, err
-		}
-
-		sources = append(sources, source)
-	}
-
-	if rows.Err() != nil {
-		return nil, mapErr(rows.Err())
-	}
-
-	return sources, nil
+	return entities.GetSources(ctx, s.db)
 }
 
 func (s source) GetAllNames(ctx context.Context) ([]string, error) {
-	const sqlGetAllNames = `SELECT name FROM sources ORDER BY name;`
-
-	rows, err := s.db.QueryContext(ctx, sqlGetAllNames)
-	if err != nil {
-		return nil, mapErr(err)
-	}
-
-	defer func() { _ = rows.Close() }()
-
-	var sourceNames []string
-	for rows.Next() {
-		var sourceName string
-		err := rows.Scan(&sourceName)
-		if err != nil {
-			return nil, err
-		}
-
-		sourceNames = append(sourceNames, sourceName)
-	}
-
-	if rows.Err() != nil {
-		return nil, mapErr(rows.Err())
-	}
-
-	return sourceNames, nil
+	return entities.GetSourceNames(ctx, s.db)
 }
 
-func (s source) GetByID(ctx context.Context, id int) (migration.Source, error) {
-	const sqlGetByID = `SELECT id, name, source_type, properties FROM sources WHERE id=:id;`
-
-	row := s.db.QueryRowContext(ctx, sqlGetByID, sql.Named("id", id))
-	if row.Err() != nil {
-		return migration.Source{}, mapErr(row.Err())
-	}
-
-	return scanSource(row)
+func (s source) GetByName(ctx context.Context, name string) (*migration.Source, error) {
+	return entities.GetSource(ctx, s.db, name)
 }
 
-func (s source) GetByName(ctx context.Context, name string) (migration.Source, error) {
-	const sqlGetByName = `SELECT id, name, source_type, properties FROM sources WHERE name=:name;`
-
-	row := s.db.QueryRowContext(ctx, sqlGetByName, sql.Named("name", name))
-	if row.Err() != nil {
-		return migration.Source{}, mapErr(row.Err())
-	}
-
-	return scanSource(row)
+func (s source) Update(ctx context.Context, in migration.Source) error {
+	return transaction.ForceTx(ctx, s.db, func(ctx context.Context, tx transaction.TX) error {
+		return entities.UpdateSource(ctx, tx, in.Name, in)
+	})
 }
 
-func (s source) UpdateByID(ctx context.Context, in migration.Source) (migration.Source, error) {
-	const sqlUpdate = `
-UPDATE sources SET name=:name, source_type=:source_type, properties=:properties
-WHERE id=:id
-RETURNING id, name, source_type, properties;
-`
-
-	row := s.db.QueryRowContext(ctx, sqlUpdate,
-		sql.Named("name", in.Name),
-		sql.Named("source_type", in.SourceType),
-		sql.Named("properties", in.Properties),
-		sql.Named("id", in.ID),
-	)
-	if row.Err() != nil {
-		return migration.Source{}, mapErr(row.Err())
-	}
-
-	return scanSource(row)
-}
-
-func scanSource(row interface{ Scan(dest ...any) error }) (migration.Source, error) {
-	var source migration.Source
-	err := row.Scan(
-		&source.ID,
-		&source.Name,
-		&source.SourceType,
-		&source.Properties,
-	)
-	if err != nil {
-		return migration.Source{}, mapErr(err)
-	}
-
-	return source, nil
+func (s source) Rename(ctx context.Context, oldName string, newName string) error {
+	return entities.RenameSource(ctx, s.db, oldName, newName)
 }
 
 func (s source) DeleteByName(ctx context.Context, name string) error {
-	const sqlDelete = `DELETE FROM sources WHERE name=:name;`
-
-	result, err := s.db.ExecContext(ctx, sqlDelete, sql.Named("name", name))
-	if err != nil {
-		return mapErr(err)
-	}
-
-	affectedRows, err := result.RowsAffected()
-	if err != nil {
-		return mapErr(err)
-	}
-
-	if affectedRows == 0 {
-		return migration.ErrNotFound
-	}
-
-	return nil
+	return entities.DeleteSource(ctx, s.db, name)
 }
