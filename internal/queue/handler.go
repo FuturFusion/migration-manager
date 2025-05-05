4da3816b9e66510bb557315aa6bca2f712e2dae9
Max Asnaashari <max.asna@futurfusion.io>
2025-05-05 09:25:56 -0700

    internal/queue: Add queue handler
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

---
+++ internal/queue/handler.go
@@ -0,0 +1,181 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	incusAPI "github.com/lxc/incus/v6/shared/api"
+
+	"github.com/FuturFusion/migration-manager/internal/migration"
+	"github.com/FuturFusion/migration-manager/internal/transaction"
+	"github.com/FuturFusion/migration-manager/internal/util"
+	"github.com/FuturFusion/migration-manager/shared/api"
+)
+
+// Handler handles interaction between services for the queue.
+type Handler struct {
+	batchLock util.IDLock[string]
+
+	batch    migration.BatchService
+	instance migration.InstanceService
+	network  migration.NetworkService
+	source   migration.SourceService
+	target   migration.TargetService
+	queue    migration.QueueService
+
+	workerLock    sync.RWMutex
+	workerUpdates map[uuid.UUID]time.Time
+}
+
+// NewMigrationHandler creates a new handler for queued migrations.
+func NewMigrationHandler(b migration.BatchService, i migration.InstanceService, n migration.NetworkService, s migration.SourceService, t migration.TargetService, q migration.QueueService) *Handler {
+	return &Handler{
+		batchLock:     util.NewIDLock[string](),
+		workerUpdates: map[uuid.UUID]time.Time{},
+
+		batch:    b,
+		instance: i,
+		network:  n,
+		source:   s,
+		target:   t,
+		queue:    q,
+	}
+}
+
+// MigrationState is a cache of all migration data for a batch, queued by instance.
+type MigrationState struct {
+	Batch  migration.Batch
+	Target migration.Target
+
+	QueueEntries map[uuid.UUID]migration.QueueEntry
+	Instances    map[uuid.UUID]migration.Instance
+	Sources      map[uuid.UUID]migration.Source
+}
+
+// RecordWorkerUpdate caches the last worker update that the corresponding instance has received.
+func (s *Handler) RecordWorkerUpdate(instanceUUID uuid.UUID) {
+	s.workerLock.Lock()
+	defer s.workerLock.Unlock()
+	s.workerUpdates[instanceUUID] = time.Now().UTC()
+}
+
+// ReceivedWorkerUpdate returns the last worker update that the corresponding instance received, if that update is within
+// the given threshold from now.
+// If no worker update is found for this instance, it returns a 404.
+func (s *Handler) ReceivedWorkerUpdate(instanceUUID uuid.UUID, threshold time.Duration) (time.Time, error) {
+	s.workerLock.RLock()
+	defer s.workerLock.RUnlock()
+	lastUpdate, ok := s.workerUpdates[instanceUUID]
+	if !ok {
+		return time.Time{}, incusAPI.StatusErrorf(http.StatusNotFound, "No worker found for instance with UUID %q", instanceUUID)
+	}
+
+	if lastUpdate.Add(threshold).Before(time.Now().UTC()) {
+		return time.Time{}, fmt.Errorf("No worker update received in %q for instance with UUID %q", threshold.String(), instanceUUID)
+	}
+
+	return lastUpdate, nil
+}
+
+// RemoveFromCache removes the given instanceUUID from the worker cache.
+func (s *Handler) RemoveFromCache(instanceUUID uuid.UUID) {
+	s.workerLock.Lock()
+	defer s.workerLock.Unlock()
+	delete(s.workerUpdates, instanceUUID)
+}
+
+// GetMigrationState fetches all migration state information corresponding to the given batch status and migration status.
+func (s *Handler) GetMigrationState(ctx context.Context, batchStatus api.BatchStatusType, migrationStatuses ...api.MigrationStatusType) (map[string]MigrationState, error) {
+	migrationState := map[string]MigrationState{}
+
+	var entries migration.QueueEntries
+	var batches migration.Batches
+	var targets migration.Targets
+	var sources migration.Sources
+	var instances migration.Instances
+	err := transaction.Do(ctx, func(ctx context.Context) error {
+		var err error
+		entries, err = s.queue.GetAllByState(ctx, migrationStatuses...)
+		if err != nil {
+			return err
+		}
+
+		batches, err = s.batch.GetAllByState(ctx, batchStatus)
+		if err != nil {
+			return err
+		}
+
+		targets, err = s.target.GetAll(ctx)
+		if err != nil {
+			return err
+		}
+
+		sources, err = s.source.GetAll(ctx)
+		if err != nil {
+			return err
+		}
+
+		instances, err = s.instance.GetAllQueued(ctx, entries)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch records for migration state: %w", err)
+	}
+
+	queueMap := map[string]map[uuid.UUID]migration.QueueEntry{}
+	for _, entry := range entries {
+		if queueMap[entry.BatchName] == nil {
+			queueMap[entry.BatchName] = map[uuid.UUID]migration.QueueEntry{}
+		}
+
+		queueMap[entry.BatchName][entry.InstanceUUID] = entry
+	}
+
+	batchMap := map[string]migration.Batch{}
+	for _, b := range batches {
+		_, ok := queueMap[b.Name]
+		if ok {
+			batchMap[b.Name] = b
+		}
+	}
+
+	srcMap := make(map[string]migration.Source, len(sources))
+	for _, s := range sources {
+		srcMap[s.Name] = s
+	}
+
+	tgtMap := make(map[string]migration.Target, len(targets))
+	for _, t := range targets {
+		tgtMap[t.Name] = t
+	}
+
+	for _, b := range batchMap {
+		state := MigrationState{
+			Batch:        b,
+			QueueEntries: queueMap[b.Name],
+			Target:       tgtMap[b.Target],
+			Instances:    map[uuid.UUID]migration.Instance{},
+			Sources:      map[uuid.UUID]migration.Source{},
+		}
+
+		for _, inst := range instances {
+			_, ok := state.QueueEntries[inst.UUID]
+			if ok {
+				state.Instances[inst.UUID] = inst
+				state.Sources[inst.UUID] = srcMap[inst.Source]
+			}
+		}
+
+		migrationState[b.Name] = state
+	}
+
+	return migrationState, nil
+}
