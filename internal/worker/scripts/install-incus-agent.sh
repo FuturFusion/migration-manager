12e566d3ba07518f5958f109fb25b195f7062acf
Max Asnaashari <max.asna@futurfusion.io>
2025-06-19 10:40:07 -0700

    internal/worker/scripts: Add SELinux labels for incus-agent
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/worker/scripts/install-incus-agent.sh
+++ internal/worker/scripts/install-incus-agent.sh
@@ -7,3 +7,13 @@ cd /mnt/config/ || exit 1
 ./install.sh
 cd /root || exit 1
 umount /mnt/config/
+
+# SELinux handling.
+if getenforce >/dev/null 2>&1 && type semanage >/dev/null 2>&1; then
+  # Add labels for the binaries and scripts executed by the incus-agent service.
+  semanage fcontext -N -a -t bin_t /var/run/incus_agent/incus-agent
+  semanage fcontext -N -a -t init_exec_t /usr/lib/systemd/incus-agent-setup
+
+  # Manually set the label for the file we already created because restorecon doesn't work in chroot.
+  chcon system_u:object_r:init_exec_t:s0 /usr/lib/systemd/incus-agent-setup
+fi
