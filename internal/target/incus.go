885dd439c76bdf60c8eece2d4f2ad83b76e68f21
Max Asnaashari <max.asna@futurfusion.io>
2025-07-02 14:49:40 -0700

    internal/target: Record source disk name as user.migration_source
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/target/incus.go
+++ internal/target/incus.go
@@ -391,10 +391,11 @@ func (t *InternalIncusTarget) fillInitialProperties(instance incusAPI.InstancesP
 
 	instance.Devices = map[string]map[string]string{
 		"root": {
-			"path":      "/",
-			"pool":      storagePool,
-			"type":      "disk",
-			sizeDef.Key: strconv.Itoa(int(p.Disks[0].Capacity)) + "B",
+			"path":                  "/",
+			"pool":                  storagePool,
+			"type":                  "disk",
+			"user.migration_source": p.Disks[0].Name,
+			sizeDef.Key:             strconv.Itoa(int(p.Disks[0].Capacity)) + "B",
 		},
 	}
 
@@ -523,7 +524,10 @@ func (t *InternalIncusTarget) CreateNewVM(instDef migration.Instance, apiDef inc
 			err := tgtClient.CreateStoragePoolVolume(storagePool, incusAPI.StorageVolumesPost{
 				StorageVolumePut: incusAPI.StorageVolumePut{
 					Description: fmt.Sprintf("Migrated disk (%s)", disk.Name),
-					Config:      map[string]string{"size": fmt.Sprintf("%dB", disk.Capacity)},
+					Config: map[string]string{
+						"size":                  fmt.Sprintf("%dB", disk.Capacity),
+						"user.migration_source": disk.Name,
+					},
 				},
 				Name:        diskName,
 				Type:        "custom",
