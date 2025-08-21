d9f11e513476fffb652a7945939891676139c66e
Max Asnaashari <max.asna@futurfusion.io>
2025-08-20 22:42:14 -0700

    internal/server/sys: Replace VMwareVIX helpers with SDK upload
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/server/sys/os.go
+++ internal/server/sys/os.go
@@ -8,12 +8,20 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	incusAPI "github.com/lxc/incus/v6/shared/api"
+	"github.com/lxc/incus/v6/shared/revert"
 
 	"github.com/FuturFusion/migration-manager/internal/util"
+	"github.com/FuturFusion/migration-manager/shared/api"
 )
 
 // OS is a high-level facade for accessing operating-system level functionalities.
 type OS struct {
+	// A lock to manage filesystem access during uploads.
+	uploadLock sync.Mutex
+
 	// Directories
 	CacheDir string // Cache directory (e.g., /var/cache/migration-manager/)
 	LogDir   string // Log directory (e.g. /var/log/).
@@ -37,6 +45,98 @@ func DefaultOS() *OS {
 	return newOS
 }
 
+// DefaultSDKPath returns the default paths for a source's sdk file, and its partial import file.
+func (s *OS) DefaultSDKPath(srcType api.SourceType) (sdkName string, sdkPartName string) {
+	sdkName = filepath.Join(s.VarDir, string(srcType), string(srcType)+".sdk")
+	sdkPartName = sdkName + ".part"
+
+	return sdkName, sdkPartName
+}
+
+// CleanPartialSDKs removes partial SDK uploads.
+func (s *OS) CleanPartialSDKs() error {
+	s.uploadLock.Lock()
+	defer s.uploadLock.Unlock()
+
+	for _, srcType := range api.VMSourceTypes() {
+		filePath, partPath := s.DefaultSDKPath(srcType)
+
+		_, err := os.Stat(partPath)
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("Failed to inspect part file for %q: %w", filePath, err)
+		}
+
+		if err == nil {
+			err := os.Remove(filePath)
+			if err != nil && !os.IsNotExist(err) {
+				return fmt.Errorf("Failed to remove incomplete sdk file %q: %w", filePath, err)
+			}
+
+			err = os.Remove(partPath)
+			if err != nil {
+				return fmt.Errorf("Failed to remove sdk part file for %q: %w", filePath, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// WriteSDK reads from the reader and writes to the default SDK path for the given source.
+// While the write is in progress, a .part file will be present.
+func (s *OS) WriteSDK(srcType api.SourceType, reader io.ReadCloser) error {
+	s.uploadLock.Lock()
+	defer s.uploadLock.Unlock()
+
+	filePath, partPath := s.DefaultSDKPath(srcType)
+
+	reverter := revert.New()
+	defer reverter.Fail()
+
+	// Create the directory if it doesn't exist yet.
+	err := os.MkdirAll(filepath.Dir(partPath), 0o755)
+	if err != nil {
+		return fmt.Errorf("Failed to create SDK source %q directory: %w", srcType, err)
+	}
+
+	// Remove any existing part files.
+	err = os.Remove(partPath)
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("Failed to delete existing SDK file for %q", srcType)
+	}
+
+	// Clear this run on errors.
+	reverter.Add(func() {
+		_ = os.Remove(partPath)
+	})
+
+	// Create a part file so we can track progress.
+	partFile, err := os.OpenFile(partPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		return fmt.Errorf("Failed to open file %q for writing: %w", partPath, err)
+	}
+
+	defer partFile.Close()
+
+	// Copy across to the file.
+	_, err = io.Copy(partFile, reader)
+	if err != nil {
+		return fmt.Errorf("Failed to write file content: %w", err)
+	}
+
+	err = os.Rename(partPath, filePath)
+	if err != nil {
+		// Target file may be corrupted, so remove it.
+		_ = os.Remove(filePath)
+
+		return fmt.Errorf("Failed to commit file: %w", err)
+	}
+
+	reverter.Success()
+
+	return nil
+}
+
 // GetUnixSocket returns the full path to the unix.socket file that this daemon is listening on.
 func (s *OS) GetUnixSocket() string {
 	path := os.Getenv("MIGRATION_MANAGER_SOCKET")
@@ -53,14 +153,19 @@ func (s *OS) LocalDatabaseDir() string {
 }
 
 // ValidateFileSystem returns whether the required and optional files have been supplied to Migration Manager.
-func (s *OS) ValidateFileSystem() error {
-	_, err := s.GetVMwareVixName()
-	if err != nil {
-		return fmt.Errorf("Failed to find VMWare vix tarball: %w", err)
+func (s *OS) ValidateFileSystem(sources ...api.SourceType) error {
+	s.uploadLock.Lock()
+	defer s.uploadLock.Unlock()
+
+	for _, src := range sources {
+		_, err := s.GetSDKName(api.SOURCETYPE_VMWARE)
+		if err != nil {
+			return fmt.Errorf("Failed to find SDK for source type %q: %w", src, err)
+		}
 	}
 
 	// Ensure exactly zero or one VirtIO drivers ISOs exist.
-	_, err = s.GetVirtioDriversISOName()
+	_, err := s.GetVirtioDriversISOName()
 	if err != nil && !os.IsNotExist(err) {
 		return fmt.Errorf("Failed to find Virtio drivers ISO: %w", err)
 	}
@@ -86,28 +191,43 @@ func (s *OS) GetVirtioDriversISOName() (string, error) {
 	return filepath.Base(files[0]), nil
 }
 
-// GetVMwareVixName returns the name of the VMWare vix disklib tarball.
-func (s *OS) GetVMwareVixName() (string, error) {
-	files, err := filepath.Glob(filepath.Join(s.VarDir, "VMware-vix-disklib*.tar.gz"))
+// GetSDKName returns the file name for a default source sdk. Returns 404 if none exist.
+// If a .part file exists, it returns an error because the sdk file is likely incomplete.
+func (s *OS) GetSDKName(srcType api.SourceType) (string, error) {
+	filePath, partPath := s.DefaultSDKPath(api.SOURCETYPE_VMWARE)
+	_, err := os.Stat(partPath)
+	if err != nil && !os.IsNotExist(err) {
+		return "", fmt.Errorf("Failed to search for in-progress SDK upload file %q: %w", partPath, err)
+	}
+
+	if err == nil {
+		return "", fmt.Errorf("SDK import in progress")
+	}
+
+	info, err := os.Stat(filePath)
+	if err != nil && !os.IsNotExist(err) {
+		return "", fmt.Errorf("Failed to inspect sdk file %q: %w", filePath, err)
+	}
+
+	// Return 404 if not found.
 	if err != nil {
-		return "", fmt.Errorf("Failed to find VMware vix tarball in %q: %w", s.VarDir, err)
+		return "", incusAPI.StatusErrorf(http.StatusNotFound, "SDK file %q not found", filePath)
 	}
 
-	if len(files) == 0 {
-		return "", os.ErrNotExist
+	if !info.Mode().IsRegular() {
+		return "", fmt.Errorf("Unexpected file mode %q", info.Mode().String())
 	}
 
-	if len(files) != 1 {
-		return "", fmt.Errorf("Failed to find exactly one VMWare vix tarball in %q (Found %d)", s.VarDir, len(files))
-	}
-
-	return filepath.Base(files[0]), nil
+	return filepath.Base(filePath), nil
 }
 
 // LoadWorkerImage writes the VMWare vix tarball to the worker image.
 // If the worker image does not exist, it is fetched from the current project version's corresponding GitHub release.
-func (s *OS) LoadWorkerImage(ctx context.Context) error {
-	vixName, err := s.GetVMwareVixName()
+func (s *OS) LoadWorkerImage(ctx context.Context, srcType api.SourceType) error {
+	s.uploadLock.Lock()
+	defer s.uploadLock.Unlock()
+
+	sdkName, err := s.GetSDKName(srcType)
 	if err != nil {
 		return err
 	}
@@ -152,12 +272,12 @@ func (s *OS) LoadWorkerImage(ctx context.Context) error {
 
 	defer rawImgFile.Close()
 
-	vixFile, err := os.Open(filepath.Join(s.VarDir, vixName))
+	sdkFile, err := os.Open(filepath.Join(s.VarDir, string(srcType), sdkName))
 	if err != nil {
 		return err
 	}
 
-	defer vixFile.Close()
+	defer sdkFile.Close()
 
 	// Move to the first partition offset.
 	_, err = rawImgFile.Seek(616448*512, io.SeekStart)
@@ -166,7 +286,7 @@ func (s *OS) LoadWorkerImage(ctx context.Context) error {
 	}
 
 	// Write the VIX tarball at the offset.
-	_, err = io.Copy(rawImgFile, vixFile)
+	_, err = io.Copy(rawImgFile, sdkFile)
 	if err != nil {
 		return err
 	}
