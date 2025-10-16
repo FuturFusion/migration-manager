2461cffe812c156a98233d1cd9c5f8fbbecb2b16
Max Asnaashari <max.asna@futurfusion.io>
2025-10-16 11:12:57 -0700

    cmd/migration-manager/internal/config: Rework cli config
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- cmd/migration-manager/internal/config/config.go
+++ cmd/migration-manager/internal/config/config.go
@@ -1,40 +1,94 @@
 package config
 
 import (
+	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 
+	incusTLS "github.com/lxc/incus/v6/shared/tls"
+	"github.com/lxc/incus/v6/shared/util"
 	"gopkg.in/yaml.v3"
 
 	"github.com/FuturFusion/migration-manager/shared/api"
 )
 
 type Config struct {
-	AuthType                   string          `yaml:"auth_type"`
-	ConfigDir                  string          `yaml:"config_dir"`
-	MigrationManagerServer     string          `yaml:"migration_manager_server"`
-	MigrationManagerServerCert api.Certificate `yaml:"migration_manager_server_cert"`
-	TLSClientCertFile          string          `yaml:"tls_client_cert_file"`
-	TLSClientKeyFile           string          `yaml:"tls_client_key_file"`
+	ConfigDir string             `yaml:"-"`
+	CertInfo  *incusTLS.CertInfo `yaml:"-"`
+
+	DefaultRemote string            `yaml:"default_remote"`
+	Remotes       map[string]Remote `yaml:"remotes"`
+}
+
+type AuthType string
+
+const (
+	AuthTypeUntrusted = AuthType("untrusted")
+	AuthTypeTLS       = AuthType("tls")
+	AuthTypeOIDC      = AuthType("oidc")
+)
+
+type Remote struct {
+	Addr       string          `yaml:"addr"`
+	AuthType   AuthType        `yaml:"auth_type"`
+	ServerCert api.Certificate `yaml:"server_cert"`
 }
 
 func NewConfig(configDir string) *Config {
 	return &Config{
 		ConfigDir: configDir,
+		Remotes:   map[string]Remote{},
 	}
 }
 
-func LoadConfig(configFile string) (*Config, error) {
-	ret := new(Config)
+func LoadConfig(configDir string) (*Config, error) {
+	ret := NewConfig(configDir)
+	if util.PathExists(ret.ConfigPath()) {
+		contents, err := os.ReadFile(ret.ConfigPath())
+		if err != nil {
+			return nil, err
+		}
 
-	contents, err := os.ReadFile(configFile)
-	if err != nil {
-		return ret, err
+		err = yaml.Unmarshal(contents, &ret)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err := ret.SaveConfig()
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	err = yaml.Unmarshal(contents, &ret)
+	for remote, config := range ret.Remotes {
+		if config.AuthType == "" {
+			config.AuthType = AuthTypeUntrusted
+		}
+
+		switch config.AuthType {
+		case AuthTypeOIDC:
+		case AuthTypeTLS:
+		case AuthTypeUntrusted:
+		default:
+			return nil, fmt.Errorf("Invalid value for config key auth_type: %v", config.AuthType)
+		}
+
+		ret.Remotes[remote] = config
+	}
+
+	// Create the OIDC token path if it doesn't exist.
+	if !util.PathExists(filepath.Dir(ret.OIDCTokenPath(""))) {
+		err := os.MkdirAll(filepath.Dir(ret.OIDCTokenPath("")), 0o750)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Initialize client certificates.
+	var err error
+	ret.CertInfo, err = ret.ClientCerts()
 	if err != nil {
-		return ret, err
+		return nil, err
 	}
 
 	return ret, nil
@@ -46,5 +100,17 @@ func (c *Config) SaveConfig() error {
 		return err
 	}
 
-	return os.WriteFile(path.Join(c.ConfigDir, "config.yml"), contents, 0o644)
+	return os.WriteFile(c.ConfigPath(), contents, 0o644)
+}
+
+func (c *Config) ConfigPath() string {
+	return filepath.Join(c.ConfigDir, "config.yml")
+}
+
+func (c *Config) OIDCTokenPath(name string) string {
+	return filepath.Join(c.ConfigDir, "oidc-tokens", name+".json")
+}
+
+func (c *Config) ClientCerts() (*incusTLS.CertInfo, error) {
+	return incusTLS.KeyPairAndCA(c.ConfigDir, "client", incusTLS.CertClient, false)
 }
