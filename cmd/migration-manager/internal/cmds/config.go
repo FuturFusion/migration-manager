94dcbec68b87c356e16aeeac0eb3e6fe455da9bd
Max Asnaashari <max.asna@futurfusion.io>
2025-10-17 14:24:30 -0700

    cmd/migration-manager/internal/cmds: Add system settings CLI
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- cmd/migration-manager/internal/cmds/config.go
+++ cmd/migration-manager/internal/cmds/config.go
@@ -35,6 +35,10 @@ func (c *CmdConfig) Command() *cobra.Command {
 	configSecurityCmd := cmdConfigSecurity{global: c.Global}
 	cmd.AddCommand(configSecurityCmd.Command())
 
+	// Settings config
+	configSettingsCmd := cmdConfigSettings{global: c.Global}
+	cmd.AddCommand(configSettingsCmd.Command())
+
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.NoArgs
 	cmd.Run = func(cmd *cobra.Command, args []string) { _ = cmd.Usage() }
@@ -89,6 +93,34 @@ func (c *cmdConfigSecurity) Command() *cobra.Command {
 
 	// Show
 	configShowCmd := cmdConfigSecurityShow{global: c.global}
+	cmd.AddCommand(configShowCmd.Command())
+
+	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
+	cmd.Args = cobra.NoArgs
+	cmd.Run = func(cmd *cobra.Command, args []string) { _ = cmd.Usage() }
+
+	return cmd
+}
+
+type cmdConfigSettings struct {
+	global *CmdGlobal
+}
+
+func (c *cmdConfigSettings) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = "settings"
+	cmd.Short = "Manage global system configuration"
+	cmd.Long = `Description:
+
+  Modify global system configuration for migration manager.
+`
+
+	// Edit
+	configEdit := cmdConfigSettingsEdit{global: c.global}
+	cmd.AddCommand(configEdit.Command())
+
+	// Show
+	configShowCmd := cmdConfigSettingsShow{global: c.global}
 	cmd.AddCommand(configShowCmd.Command())
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
@@ -315,6 +347,122 @@ func (c *cmdConfigSecurityShow) Run(cmd *cobra.Command, args []string) error {
 	}
 
 	var cfg api.SystemSecurity
+	err = responseToStruct(resp, &cfg)
+	if err != nil {
+		return err
+	}
+
+	b, err := yaml.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(b))
+
+	return nil
+}
+
+type cmdConfigSettingsEdit struct {
+	global *CmdGlobal
+}
+
+func (c *cmdConfigSettingsEdit) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = "edit"
+	cmd.Short = "Edit settings config as YAML"
+
+	cmd.RunE = c.Run
+
+	return cmd
+}
+
+func (c *cmdConfigSettingsEdit) helpTemplate() string {
+	return `### This is a YAML representation of the system settings configuration.
+### Any line starting with a '# will be ignored.
+###`
+}
+
+func (c *cmdConfigSettingsEdit) Run(cmd *cobra.Command, args []string) error {
+	exit, err := c.global.CheckArgs(cmd, args, 0, 0)
+	if exit {
+		return err
+	}
+
+	// If stdin isn't a terminal, read text from it
+	var contents []byte
+	if !termios.IsTerminal(getStdinFd()) {
+		contents, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+	} else {
+		resp, _, err := c.global.doHTTPRequestV1("/system/settings", http.MethodGet, "", nil)
+		if err != nil {
+			return err
+		}
+
+		var cfg api.SystemSettings
+		err = responseToStruct(resp, &cfg)
+		if err != nil {
+			return err
+		}
+
+		data, err := yaml.Marshal(cfg)
+		if err != nil {
+			return err
+		}
+
+		contents, err = textEditor([]byte(c.helpTemplate() + "\n\n" + string(data)))
+		if err != nil {
+			return err
+		}
+	}
+
+	newdata := api.SystemSettings{}
+	err = yaml.Unmarshal(contents, &newdata)
+	if err != nil {
+		return err
+	}
+
+	b, err := json.Marshal(newdata)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = c.global.doHTTPRequestV1("/system/settings", http.MethodPut, "", b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type cmdConfigSettingsShow struct {
+	global *CmdGlobal
+}
+
+func (c *cmdConfigSettingsShow) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = "show"
+	cmd.Short = "Display the system settings config"
+
+	cmd.RunE = c.Run
+
+	return cmd
+}
+
+func (c *cmdConfigSettingsShow) Run(cmd *cobra.Command, args []string) error {
+	exit, err := c.global.CheckArgs(cmd, args, 0, 0)
+	if exit {
+		return err
+	}
+
+	resp, _, err := c.global.doHTTPRequestV1("/system/settings", http.MethodGet, "", nil)
+	if err != nil {
+		return err
+	}
+
+	var cfg api.SystemSettings
 	err = responseToStruct(resp, &cfg)
 	if err != nil {
 		return err
