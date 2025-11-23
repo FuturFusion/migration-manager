d3acd26869529896ac6fa528e2551b8e1d830245
Stéphane Graber <stephane.graber@futurfusion.io>
2025-11-22 20:49:35 -0500

    Release 0.1.0
    Signed-off-by: Stéphane Graber <stephane.graber@futurfusion.io>

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,6 +1,6 @@
 package version
 
-const Version string = "0.0.0-pre.5"
+const Version string = "0.1.0"
 
 // GoVersion returns the project version formatted as a Go semantic version string.
 func GoVersion() string {
