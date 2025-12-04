753a9e855935250e3ba036d288857c9adfdb4659
Max Asnaashari <max.asna@futurfusion.io>
2025-12-04 15:43:59 -0800

    shared/api/events: Generate lifecycle helpers for each entity
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

---
+++ generate.go
@@ -0,0 +1,4 @@
+package tools
+
+//go:generate go run github.com/FuturFusion/migration-manager/cmd/generate-event --target shared/api/event
+//go:generate go run golang.org/x/tools/cmd/goimports -w shared/api/event
