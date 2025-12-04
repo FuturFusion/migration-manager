7f0c1181853ca5dae95f4aed62515fa868dcd3a9
Max Asnaashari <max.asna@futurfusion.io>
2025-12-04 15:43:58 -0800

    internal/migration: Return updated batch from service methods
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/migration/batch_ports.go
+++ internal/migration/batch_ports.go
@@ -21,9 +21,9 @@ type BatchService interface {
 	UpdateStatusByName(ctx context.Context, name string, status api.BatchStatusType, statusMessage string) (*Batch, error)
 	Rename(ctx context.Context, oldName string, newName string) error
 	DeleteByName(ctx context.Context, name string) error
-	StartBatchByName(ctx context.Context, name string, windowSvc WindowService, networkSvc NetworkService, queueSvc QueueService) error
-	StopBatchByName(ctx context.Context, name string) error
-	ResetBatchByName(ctx context.Context, name string, queueSvc QueueService, sourceSvc SourceService, targetSvc TargetService) error
+	StartBatchByName(ctx context.Context, name string, windowSvc WindowService, networkSvc NetworkService, queueSvc QueueService) (*Batch, error)
+	StopBatchByName(ctx context.Context, name string) (*Batch, error)
+	ResetBatchByName(ctx context.Context, name string, queueSvc QueueService, sourceSvc SourceService, targetSvc TargetService) (*Batch, error)
 
 	DeterminePlacement(ctx context.Context, instance Instance, usedNetworks Networks, batch Batch, windows Windows) (*api.Placement, error)
 }
