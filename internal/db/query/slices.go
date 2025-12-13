581a072548b46fabf1a6cffed9a7f02e5b35caeb
Max Asnaashari <max.asna@futurfusion.io>
2025-12-13 07:25:44 -0800

    internal/db/schema: Add SchemaDotGo
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/db/query/slices.go
+++ internal/db/query/slices.go
@@ -28,6 +28,29 @@ func SelectIntegers(ctx context.Context, tx *sql.Tx, query string, args ...any) 
 	return values, nil
 }
 
+// SelectStrings executes a statement which must yield rows with a single string
+// column. It returns the list of column values.
+func SelectStrings(ctx context.Context, tx *sql.Tx, query string, args ...any) ([]string, error) {
+	values := []string{}
+	scan := func(rows *sql.Rows) error {
+		var value string
+		err := rows.Scan(&value)
+		if err != nil {
+			return err
+		}
+
+		values = append(values, value)
+		return nil
+	}
+
+	err := scanSingleColumn(ctx, tx, query, args, scan)
+	if err != nil {
+		return nil, err
+	}
+
+	return values, nil
+}
+
 // Execute the given query and ensure that it yields rows with a single column
 // of the given database type. For every row yielded, execute the given
 // scanner.
--- internal/db/update.go
+++ internal/db/update.go
@@ -14,79 +14,22 @@ import (
 	"github.com/FuturFusion/migration-manager/shared/api"
 )
 
-const freshSchema = `
-CREATE TABLE batches (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    target_id INTEGER NOT NULL,
-    target_project VARCHAR(255) NOT NULL,
-    status TEXT NOT NULL,
-    status_message TEXT NOT NULL,
-    storage_pool VARCHAR(255) NOT NULL,
-    include_expression TEXT NOT NULL,
-    migration_window_start DATETIME NOT NULL,
-    migration_window_end DATETIME NOT NULL,
-    UNIQUE (name),
-    FOREIGN KEY(target_id) REFERENCES targets(id)
-);
-
-CREATE TABLE instances (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    uuid TEXT NOT NULL,
-    migration_status TEXT NOT NULL,
-    migration_status_message TEXT NOT NULL,
-    last_update_from_source DATETIME NOT NULL,
-    last_update_from_worker DATETIME NOT NULL,
-    source_id INTEGER NOT NULL,
-    batch_id INTEGER NULL,
-    needs_disk_import INTEGER NOT NULL,
-    secret_token TEXT NOT NULL,
-    properties TEXT NOT NULL,
-    UNIQUE (uuid),
-    FOREIGN KEY(source_id) REFERENCES sources(id),
-    FOREIGN KEY(batch_id) REFERENCES batches(id)
-);
-
-CREATE TABLE instance_overrides (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    uuid TEXT NULL,
-    last_update DATETIME NOT NULL,
-    comment TEXT NOT NULL,
-    disable_migration INTEGER NOT NULL,
-    properties TEXT NOT NULL,
-    UNIQUE (uuid),
-    FOREIGN KEY(uuid) REFERENCES instances(uuid)
-);
-
-CREATE TABLE networks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    config INTEGER NOT NULL,
-    UNIQUE (name)
-);
-
-CREATE TABLE sources (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    source_type TEXT NOT NULL,
-    properties TEXT NOT NULL,
-    UNIQUE (name)
-);
-
-CREATE TABLE targets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    target_type TEXT NOT NULL,
-    properties TEXT NOT NULL,
-    UNIQUE (name)
-);
-`
-
 // Schema for the local database.
 func Schema() *schema.Schema {
 	dbSchema := schema.NewFromMap(updates)
-
+	dbSchema.Fresh(freshSchema)
 	return dbSchema
+}
+
+// FreshSchema returns the fresh schema definition of the global database.
+func FreshSchema() string {
+	return freshSchema
+}
+
+// SchemaDotGo refreshes the schema.go file in this package, using the updates
+// defined here.
+func SchemaDotGo() error {
+	return schema.DotGo(updates, "schema")
 }
 
 /* Database updates are one-time actions that are needed to move an
@@ -883,7 +826,73 @@ ALTER TABLE networks_new RENAME TO networks;
 
 func updateFromV0(ctx context.Context, tx *sql.Tx) error {
 	// v0..v1 the dawn of migration manager
-	stmt := freshSchema
+	stmt := `
+CREATE TABLE batches (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    target_id INTEGER NOT NULL,
+    target_project VARCHAR(255) NOT NULL,
+    status TEXT NOT NULL,
+    status_message TEXT NOT NULL,
+    storage_pool VARCHAR(255) NOT NULL,
+    include_expression TEXT NOT NULL,
+    migration_window_start DATETIME NOT NULL,
+    migration_window_end DATETIME NOT NULL,
+    UNIQUE (name),
+    FOREIGN KEY(target_id) REFERENCES targets(id)
+);
+
+CREATE TABLE instances (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    uuid TEXT NOT NULL,
+    migration_status TEXT NOT NULL,
+    migration_status_message TEXT NOT NULL,
+    last_update_from_source DATETIME NOT NULL,
+    last_update_from_worker DATETIME NOT NULL,
+    source_id INTEGER NOT NULL,
+    batch_id INTEGER NULL,
+    needs_disk_import INTEGER NOT NULL,
+    secret_token TEXT NOT NULL,
+    properties TEXT NOT NULL,
+    UNIQUE (uuid),
+    FOREIGN KEY(source_id) REFERENCES sources(id),
+    FOREIGN KEY(batch_id) REFERENCES batches(id)
+);
+
+CREATE TABLE instance_overrides (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    uuid TEXT NULL,
+    last_update DATETIME NOT NULL,
+    comment TEXT NOT NULL,
+    disable_migration INTEGER NOT NULL,
+    properties TEXT NOT NULL,
+    UNIQUE (uuid),
+    FOREIGN KEY(uuid) REFERENCES instances(uuid)
+);
+
+CREATE TABLE networks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    config INTEGER NOT NULL,
+    UNIQUE (name)
+);
+
+CREATE TABLE sources (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    source_type TEXT NOT NULL,
+    properties TEXT NOT NULL,
+    UNIQUE (name)
+);
+
+CREATE TABLE targets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    target_type TEXT NOT NULL,
+    properties TEXT NOT NULL,
+    UNIQUE (name)
+);
+`
 	_, err := tx.Exec(stmt)
 	return MapDBError(err)
 }
