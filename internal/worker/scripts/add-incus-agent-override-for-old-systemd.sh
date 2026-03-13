c390d70d72743b9d648f6caadc78f861f87251eb
Max Asnaashari <max.asna@futurfusion.io>
2026-03-13 16:57:14 -0700

    internal/worker/scripts: Relax SELinux permissions on older RHEL derivatives
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/worker/scripts/add-incus-agent-override-for-old-systemd.sh
+++ internal/worker/scripts/add-incus-agent-override-for-old-systemd.sh
@@ -12,3 +12,14 @@ WorkingDirectory=
 ExecStart=
 ExecStart=/bin/sh -c "cd /run/incus_agent/; exec ./incus-agent"
 EOF
+
+# Older SELinux causes errors with Type=notify, so make systemd_notify_t more permissive.
+# The abstract unix socket is also unlabeled, so we need to allow initrc communication explicitly.
+if getenforce >/dev/null 2>&1 ; then
+  cat > /tmp/incus_agent.cil << EOF
+(allow systemd_notify_t kernel_t (unix_dgram_socket (sendto)))
+(allow initrc_t unlabeled_t (socket (getopt read write ioctl)))
+EOF
+
+  semodule -i /tmp/incus_agent.cil
+fi
