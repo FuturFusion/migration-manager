08f9c374060b12b6f8a6cfdba4adad72872b6f7c
Abiola Ibrahim <abiola.ibrahim@futurfusion.io>
2026-08-21 15:48:23 +0100

    internal/migration: Update disabled reasons for static evaluation
    Signed-off-by: Abiola Ibrahim <abiola.ibrahim@futurfusion.io>

--- internal/migration/errors.go
+++ internal/migration/errors.go
@@ -6,10 +6,8 @@ import (
 )
 
 var (
-	ErrNotFound = errors.New("Not found")
-
-	ErrConstraintViolation = errors.New("Constraint violation")
-
+	ErrNotFound              = errors.New("Not found")
+	ErrConstraintViolation   = errors.New("Constraint violation")
 	ErrOperationNotPermitted = errors.New("Operation not permitted")
 )
 
@@ -22,3 +20,46 @@ func NewValidationErrf(format string, a ...any) error {
 func (e ErrValidation) Error() string {
 	return string(e)
 }
+
+// ErrDisabled is returned when an instance is disabled.
+type ErrDisabled struct {
+	err    error
+	reason DisabledReason
+}
+
+// Error implements error.Error.
+func (e ErrDisabled) Error() string {
+	return e.err.Error()
+}
+
+// Unwrap returns the wrapped error, so errors.Is and errors.As can inspect it.
+func (e ErrDisabled) Unwrap() error {
+	return e.err
+}
+
+// Reason returns the underlying reason for the error.
+func (e ErrDisabled) Reason() DisabledReason {
+	return e.reason
+}
+
+// NewDisabledErrf creates a new error using the specified reason and format.
+func NewDisabledErrf(reason DisabledReason, format string, a ...any) error {
+	return ErrDisabled{
+		err:    fmt.Errorf(format, a...),
+		reason: reason,
+	}
+}
+
+// DisabledReason is the reason behind the error for a disabled instance.
+type DisabledReason string
+
+const (
+	DISABLEDREASON_MANUALLY_DISABLED             DisabledReason = "Manually disabled"
+	DISABLEDREASON_INVALID_HOSTNAME              DisabledReason = "Invalid hostname"
+	DISABLEDREASON_UNKNOWN_OS                    DisabledReason = "Unknown OS"
+	DISABLEDREASON_UNKNOWN_ARCHITECTURE          DisabledReason = "Unknown architecture"
+	DISABLEDREASON_UNKNOWN_IP_ADDRESS            DisabledReason = "Unknown IP address"
+	DISABLEDREASON_VERIFYING_BACKGROUND_IMPORT   DisabledReason = "Verifying background import"
+	DISABLEDREASON_UNSUPPORTED_BACKGROUND_IMPORT DisabledReason = "Unsupported background import"
+	DISABLEDREASON_UNSUPPORTED_DISK_SNAPSHOT     DisabledReason = "Unsupported disk snapshot"
+)
